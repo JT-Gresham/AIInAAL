bec467872b661e0db2c33a5ab6e8b5d074e57e7b
OCD Insomniac <119110518+JT-Gresham@users.noreply.github.com>
2024-06-03 09:04:03 -0400

    Initial Framework

---
+++ AIInAAL_install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+echo "Installation of the AIInAAL framework"
+echo "What directory do you want the AIInAAL framework to be installed in?"
+echo "- Enter the FULL PATH below. Do NOT include the \"/\" at the very end."
+read pdirectory
+echo "Downloading AIInAAL..."
+cd $pdirectory
+git clone "http://github.com/JT-Gresham/AIInAAL"
+#Run after git pull
+sed -i 's|ParentDirectory|$pdirectory|g' AIInAAL/libref
