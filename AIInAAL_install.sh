c03a63f11f7d9aef57b65f2915b10e27ebd91816
OCD Insomniac <119110518+JT-Gresham@users.noreply.github.com>
2024-06-04 15:55:10 -0400

    bug fix

--- AIInAAL_install.sh
+++ AIInAAL_install.sh
@@ -35,7 +35,8 @@ echo ""
 git clone https://github.com/JT-Gresham/AIInAAL.git
 echo ""
 cd $pdirectory/AIInAAL/Scripts
-touch -p /etc/AIInAAL/AIInAAL_path
+mkdir /etc/AIInAAL
+touch /etc/AIInAAL/AIInAAL_path
 echo "#!/usr/bin/env bash" > /etc/AIInAAL/AIInAAL_path
 echo "" > /etc/AIInAAL/AIInAAL_path
 echo "pdirectory=$pdirectory" > /etc/AIInAAL/AIInAAL_path
