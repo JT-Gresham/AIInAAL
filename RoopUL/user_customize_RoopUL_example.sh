4d3a53e4b233e378d232104961813383e7430e39
OCD Insomniac <119110518+JT-Gresham@users.noreply.github.com>
2024-10-04 16:07:27 -0400

    Initial RoopUL installer work.

---
+++ RoopUL/user_customize_RoopUL_example.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+source AIInAAL/libref
+####  This file is for you to add any code you want to execute just before RoopUL launches.  This will not be updated when AIInAAL or RoopUL updates.
+####  This is useful if you want to create custom presets and make sure your code, links, etc gets put back after an official update from the RoopUL team.
+####  This is the example file. Activate by removing "_example" from the file name.
+
+####  THIS CODE STARTS IN YOUR PARENT DIRECTORY
+####    eg: where the AIInAAL directory is located
