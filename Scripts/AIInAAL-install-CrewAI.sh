d1d110881afc14ce84da18cadfe8ea5937f6412f
OCD Insomniac <119110518+JT-Gresham@users.noreply.github.com>
2024-10-06 17:10:00 -0400

    CrewAI work WIP

--- Scripts/AIInAAL-install-CrewAI.sh
+++ Scripts/AIInAAL-install-CrewAI.sh
@@ -24,12 +24,11 @@ echo ""
 echo "Cloning official $aiinaalpkg repository to $aiinaalpkg"
 
 #### GIT CLONE COMMAND  URL HERE ####
-mkdir "/tmp/$aiinaalpkg"
-cd "/tmp/$aiinaalpkg"
+cd /tmp
 wget -O - https://github.com/crewAIInc/crewAI/archive/master.tar.gz | tar -xz --strip=2 "crewAI-main/src/crewai"
 mv ./crewai -t ./CrewAI
 cd $AIInAALdir
-mv -f "/tmp/$aiinaalpkg/CrewAI" -t ./
+mv -f "/tmp/$aiinaalpkg" -t ./
 cd $AIInAALdir/$aiinaalpkg
 #rm -r /tmp/$aiinaalpkg
 source $AIInAALdir/$aiinaalpkg/libref-$aiinaalpkg
