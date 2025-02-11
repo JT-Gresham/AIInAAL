720c6b79308ecafd07f865451a870a98e82e93ed
OCD Insomniac <119110518+JT-Gresham@users.noreply.github.com>
2025-02-11 15:02:03 -0500

    ComfyUI - Update installer to autoinstall ComfyUI-Manager

--- Scripts/AIInAAL-install-ComfyUI.sh
+++ Scripts/AIInAAL-install-ComfyUI.sh
@@ -43,7 +43,10 @@ pip install -r requirements.txt
 pip install -r requirements_$aiinaalpkg.txt
 echo ""
 echo "Creating the launcher file ($aiinaalpkg-Start.sh)"
-cp user_customize_$aiinaalpkg_example.sh user_customize_$aiinaalpkg.sh
+cp "user_customize_"$aiinaalpkg"_example.sh" "user_customize_"$aiinaalpkg".sh"
+cd custom_nodes
+git clone https://github.com/ltdrdata/ComfyUI-Manager comfyui-manager
+cd ..
 touch $AIInAALdir/$aiinaalpkg/$aiinaalpkg-Start.sh
 echo "#!/usr/bin/env bash" > $AIInAALdir/$aiinaalpkg/$aiinaalpkg-Start.sh
 echo "" >> $AIInAALdir/$aiinaalpkg/$aiinaalpkg-Start.sh
