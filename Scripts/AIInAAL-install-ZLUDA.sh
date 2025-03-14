910729c0deb48193e2fc19690379166d4e115d2c
JT Gresham <j.gresham14@gmail.com>
2025-03-14 09:45:10 -0400

    Try to add ZLUDA as cuda replacement

---
+++ Scripts/AIInAAL-install-ZLUDA.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+mkdir $AIInAALdir/AIInAAL_env/lib/python3.11/site-packages/ZLUDA
+cd $AIInAALdir/AIInAAL_env/lib/python3.11/site-packages/ZLUDA
+git clone --recursive https://github.com/vosen/ZLUDA.git
+
+
