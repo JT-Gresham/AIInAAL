9879bd54c7e47af093ddc418b7b2a9213fe021dc
JT Gresham <j.gresham14@gmail.com>
2025-03-14 09:50:22 -0400

    Fix directory issue with ZLUDA installer

--- Scripts/AIInAAL-install-ZLUDA.sh
+++ Scripts/AIInAAL-install-ZLUDA.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
-mkdir $AIInAALdir/AIInAAL_env/lib/python3.11/site-packages/ZLUDA
-cd $AIInAALdir/AIInAAL_env/lib/python3.11/site-packages/ZLUDA
+source /etc/AIInAAL/AIInAAL_path
+source $AIInAALdir/libref
+source $AIInAALdir/AIInAAL_env/bin/activate
+
+cd $AIInAALdir/AIInAAL_env/lib/python3.11/site-packages
 git clone --recursive https://github.com/vosen/ZLUDA.git
 
 
