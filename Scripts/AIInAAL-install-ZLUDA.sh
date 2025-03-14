a537145ee2e0374b5909e90249c09ea5b5801704
JT Gresham <j.gresham14@gmail.com>
2025-03-14 10:38:58 -0400

    Add version 2 files to Scripts directory.

--- Scripts/AIInAAL-install-ZLUDA.sh
+++ Scripts/AIInAAL-install-ZLUDA.sh
@@ -4,7 +4,10 @@ source /etc/AIInAAL/AIInAAL_path
 source $AIInAALdir/libref
 source $AIInAALdir/AIInAAL_env/bin/activate
 
+sudo pacman -S --needed cargo-run-bin cargo-release
 cd $AIInAALdir/AIInAAL_env/lib/python3.11/site-packages
 git clone --recursive https://github.com/vosen/ZLUDA.git
+cd ZLUDA
+cargo xtask --release
 
 
