c395d420be3e2742cf1cb58255f32b157e425b21
JT Gresham <j.gresham14@gmail.com>
2025-03-19 10:41:31 -0400

    Exo Initial work

---
+++ Scripts/AIInAAL-install-Exo.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+source /etc/AIInAAL/AIInAAL_path
+AIInAALuser="$(whoami)"
+
+echo "##### AIInAAL installation of EXO #####"
+cd $AIInAALdir
+echo ""
+echo "Creating python 3.12 environment (AIInAAL12_env)"
+/usr/bin/python3.12 -m venv $pdirectory/AIInAAL/AIInAAL12_env
+echo ""
+echo "Activating environment -> AIInAAL12_env"
+source $pdirectory/AIInAAL/AIInAAL12_env/bin/activate
+echo "AIInAAL installer needs to check/install some PACMAN packages...Please authorize to continue..."
+sudo pacman -r $pdirectory/AIInAAL/AIInAAL12_env/ -S --needed intel-compute-runtime intel-graphics-compiler ocl-icd opencl-headers level-zero-loader python-pipx lm_sensors
+echo "Installing pip..."
+cd $pdirectory/AIInAAL/Scripts
+python get-pip.py
+cd $pdirectory/AIInAAL
+echo ""
+echo "Installing wheel package..."
+pip install wheel
+
+ln -sf $pdirectory/AIInAAL/Scripts/ipex-llm-init $pdirectory/AIInAAL/AIInAAL12_env/bin/
+#ln -sf $pdirectory/AIInAAL/Scripts/ipex-llm-init $pdirectory/AIInAAL/AIInAAL12_env_inf/bin/
+echo ""
+echo "Initializing the AIInAAL and Deb4AIInAAL(12) commands."
+ln -sf $pdirectory/AIInAAL/Scripts/AIInAAL $pdirectory/AIInAAL/AIInAAL12_env/bin/AIInAAL
+sudo ln -sf $pdirectory/AIInAAL/AIInAAL12_env/bin/AIInAAL /bin/AIInAAL
+sudo ln -sf $pdirectory/AIInAAL/AIInAAL12_env/bin/Deb4AIInAAL12 /bin/Deb4AIInAAL12
+#sudo chmod +x /bin/AIInAAL
+sudo chmod +x $pdirectory/AIInAAL/AIInAAL12_env/bin/AIInAAL
+sudo chmod +x $pdirectory/AIInAAL/AIInAAL12_env/bin/Deb4AIInAAL12
+echo "Creating initial \"shared\" directory...replace with yours afterward, if necssary (symlink allowed)"
+if [ ! -d "$pdirectory/AIInAAL/shared" ]; then
+  mv ./shared1 ./shared
+fi
+cd $pdirectory/AIInAAL
+git clone https://github.com/exo-explore/exo.git Exo
+cd $AIInAALdir/Exo
+pip install -e .
+echo "AIInAAL framework is now installed. AI program installers are located in the \"Scripts\" directory."
+echo "AIInAAL is updated \(if necessary\) whenever a framework program is started, however..."
+echo "   you can also use the command: \"AIInAAL-update\" in your terminal to update AIInAAL whenever you wish."
+echo ""
+echo "Thank you for trying out AIInAAL...this is a lot of work."
+echo ""
+echo "Before you press a key to exit the installer, a small request:"
+echo "   I'd appreciate it if you'd consider supporting my work by using one of the methods listed on the main github page. - OCD"
+read go
+#Run after git pull
