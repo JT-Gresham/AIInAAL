3a3503aee88e31b954f481fca5a26776be48b910
JT Gresham <j.gresham14@gmail.com>
2025-03-24 19:05:55 -0400

    Work on Ollama portable integration and the AIInAAL browser

--- Ollama/user_customize_Ollama_example.sh
+++ Ollama/user_customize_Ollama_example.sh
@@ -13,3 +13,39 @@ export OLLAMA_NUM_GPU=999
 #  Change directory to make things easier
 #cd $pdirectory/AIInAAL
 
+##############################################
+# AUTOSTART Open WebUI in browser and DAEMON #
+
+browserchoice="firefox"
+
+while [ 1 ]
+do
+ollamabinpid=$(pgrep -f "ollama-bin serve")
+ollamabinmem=$(RSSfull=$(cat /proc/$ollamabinpid/status | grep RSS); echo $RSSfull | cut -d\  -f2)
+if [[ $ollamabinmem -ge "1" ]]
+	then
+		echo "Ollama-bin Activated!"
+		sleep 3
+		open-webui serve &
+		break
+	else
+		echo "Ollama-bin activation check: Waiting..."
+		sleep 3
+fi
+done
+
+while [ 1 ]
+do
+openwebuipid=$(pgrep -f "open-webui serve")
+openwebuimem=$(RSSfull=$(cat /proc/$openwebuipid/status | grep RSS); echo $RSSfull | cut -d\  -f2)
+if [[ $openwebuimem -ge "1751916" ]]
+	then
+		echo "OpenWebUI Activated!"
+		sleep 3
+		firefox "http://0.0.0.0:8080/auth/"
+		break
+	else
+		echo "OpenWebUI activation: Waiting..."
+		sleep 3
+fi
+done
