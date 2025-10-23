a454308a64b07ea1f67a9635f9d926cd3bb2af4b
JT Gresham <j.gresham14@gmail.com>
2025-10-22 20:11:12 -0400

    Adjust requirements for ComfyUI. Revert executable string.

--- Scripts/AIInAAL-install-ComfyUI.sh
+++ Scripts/AIInAAL-install-ComfyUI.sh
@@ -67,12 +67,12 @@ echo "" >> $AIInAALdir/$aiinaalpkg/$aiinaalpkg-Start.sh
 echo "#export TORCH_DEVICE_BACKEND_AUTOLOAD=0" >> $AIInAALdir/$aiinaalpkg/$aiinaalpkg-Start.sh
 echo "" >> $AIInAALdir/$aiinaalpkg/$aiinaalpkg-Start.sh
 echo "# DEFAULT Executable" >> $AIInAALdir/$aiinaalpkg/$aiinaalpkg-Start.sh
-echo "#python $AIInAALdir/$aiinaalpkg/main.py --port 7865 \"\$@\"" >> $AIInAALdir/$aiinaalpkg/$aiinaalpkg-Start.sh
+echo "python $AIInAALdir/$aiinaalpkg/main.py --port 7865 \"\$@\"" >> $AIInAALdir/$aiinaalpkg/$aiinaalpkg-Start.sh
 echo "" >> $AIInAALdir/$aiinaalpkg/$aiinaalpkg-Start.sh
 echo "# Enables split attention and async offload (use this for oneapi 2025.1)" >> $AIInAALdir/$aiinaalpkg/$aiinaalpkg-Start.sh
 echo "#export UR_L0_ENABLE_SYSMAN_ENV_DEFAULT=0" >> $AIInAALdir/$aiinaalpkg/$aiinaalpkg-Start.sh
 echo "#python $AIInAALdir/$aiinaalpkg/main.py --use-split-cross-attention --async-offload --port 7865 \"\$@\"" >> $AIInAALdir/$aiinaalpkg/$aiinaalpkg-Start.sh
-echo "python /home/LACII14/Archive-M1/AI/AIInAAL/ComfyUI/main.py --novram --bf16-unet --cpu-vae --use-split-cross-attention --disable-smart-memory --async-offload --oneapi-device-selector level_zero:gpu --port 7865 "$@"
+echo "#python /home/LACII14/Archive-M1/AI/AIInAAL/ComfyUI/main.py --novram --bf16-unet --cpu-vae --use-split-cross-attention --disable-smart-memory --async-offload --oneapi-device-selector level_zero:gpu --port 7865 "$@"
 " >> $AIInAALdir/$aiinaalpkg/$aiinaalpkg-Start.sh
 
 echo "sleep 60"
